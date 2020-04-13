abb45507542712cd06b398f9f8eaeebe76ee72af
NikitaStudent <32135393+NikitaStudent@users.noreply.github.com>
2020-04-13 18:34:56 +0300

    Initial Commit

---
+++ AccountingWater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccountingWater
+//
+//  Created by Никита on 13.04.2020.
+//  Copyright © 2020 beet.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
