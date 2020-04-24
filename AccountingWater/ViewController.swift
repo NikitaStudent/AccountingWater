aa57f19cb0264e96ef8d10ecc4026ce59cb63e51
NikitaStudent <nikitakozhevnikov2k@icloud.com>
2020-04-24 09:39:46 +0300

    Add 3 button's, and 2 View Controller

--- AccountingWater/ViewController.swift
+++ AccountingWater/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello,world")
+       
+      
     }
 
-
+    @IBAction func AddButton(_ sender: Any) {
+        
+    }
+    
+    
 }
 
