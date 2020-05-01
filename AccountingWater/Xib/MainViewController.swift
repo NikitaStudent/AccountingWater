a6365b362bf04e964ab453003d8a2fbcf39c43b8
NikitaStudent <nikitakozhevnikov2k@icloud.com>
2020-05-01 15:31:18 +0300

    Update files

--- AccountingWater/Xib/MainViewController.swift
+++ AccountingWater/Xib/MainViewController.swift
@@ -2,30 +2,21 @@
 //  MainViewController.swift
 //  AccountingWater
 //
-//  Created by Никита on 29.04.2020.
+//  Created by Никита on 01.05.2020.
 //  Copyright © 2020 beet.inc. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: ViewController {
 
-    let Speed : Float = 10
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Main"
+
+        // Do any additional setup after loading the view.
     }
+
 
     
-    @IBAction func NormalButton(_ sender: Any) {
-        let AdditionalViewController = AddViewController()
-        navigationController?.pushViewController(AdditionalViewController, animated: true)
-    }
-    @IBOutlet weak var ProgressView: UIProgressView!
-    @IBAction func CoffeeButton(_ sender: Any) {
-
-    }
-    
-
 
 }
